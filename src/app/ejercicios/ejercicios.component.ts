4cdd752f0e8d8f22be3b744d4fbdedb09d8f0e35
dlunaa <dlunaa@uoc.edu>
2020-07-14 12:08:34 +0200

    Se agrego componente y libreria

---
+++ src/app/ejercicios/ejercicios.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Ejercicio } from '../ejercicio';
+
+@Component({
+  selector: 'app-ejercicios',
+  templateUrl: './ejercicios.component.html',
+  styleUrls: ['./ejercicios.component.css']
+})
+export class EjerciciosComponent implements OnInit {
+
+  ejercicio: Ejercicio = {
+    id: 1,
+    name:"Flexion"
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
